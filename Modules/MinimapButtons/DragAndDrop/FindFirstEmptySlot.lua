4bb9b8209252a2b62d5428f6a66a173664b463c2
Kijascha <keyara@gmx.de>
2021-09-28 19:07:04 +0200

    Method FindFirstEmptySlot implemented

---
+++ Modules/MinimapButtons/DragAndDrop/FindFirstEmptySlot.lua
@@ -0,0 +1,19 @@
+local KYA, E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local MB = KYA:GetModule('MinimapButtons');
+
+if not MB.DragAndDrop then return end
+
+function MB.DragAndDrop:FindFirstEmptySlot(frame)
+    if not frame:GetName() == "KyaUI_ButtonGrid" then return end
+
+    local slot = nil;
+    
+    for i = 1, #frame.Buttons do
+        if frame.Buttons[i].isEmpty then 
+            slot = frame.Button[i];
+            break;
+        end
+    end    
+    
+    return slot;
+end
