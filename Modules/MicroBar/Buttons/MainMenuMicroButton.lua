46bd7c3a524e6b78e66ebe7939f5729e30103436
Uwe Prehn <keyara@gmx.de>
2022-05-25 15:20:47 +0200

    Full integration of BugSack within the microbutton

--- Modules/MicroBar/Buttons/MainMenuMicroButton.lua
+++ Modules/MicroBar/Buttons/MainMenuMicroButton.lua
@@ -18,6 +18,35 @@ local GetNetStats = GetNetStats
 if not MB.MicroButtons then MB.MicroButtons = {} end;
 local gradientPacks = MB.Utils.Colors.GradientPacks;
 
+local function BugSack_LDB_OnClick(self, button)
+	if not KYA.BS then return end
+
+	local addon = BugSack
+	local addonName = "BugSack"
+	local addonFrame = BugSackFrame
+
+	local success = false 
+
+	if IsControlKeyDown() and button == "RightButton" then
+		InterfaceOptionsFrame_OpenToCategory(addonName)
+		--InterfaceOptionsFrame_OpenToCategory(addonName)
+		success = true
+	elseif (IsControlKeyDown() and button == "LeftButton") and (addonFrame and addonFrame:IsShown()) then
+		addon:CloseSack()
+		success = true
+	elseif IsControlKeyDown() and button == "LeftButton" then
+		addon:OpenSack()
+		success = true
+	elseif IsShiftKeyDown() then
+		ReloadUI()
+		success = true
+	elseif IsAltKeyDown() and (addon.db.altwipe == true) then
+		addon:Reset()
+		success = true
+	end
+	return success;
+end
+
 function MB.MicroButtons:MainMenuMicroButton(button)
     if button.Name == "MainMenuMicroButton" then
 		button.Texture:SetTexture(MB.Utils:GetMicroBarIcon("MainMenu"))		
@@ -36,28 +65,39 @@ function MB.MicroButtons:MainMenuMicroButton(button)
 				button.isMouseDown = false	
 			end)
 		end
-		button:SetScript('OnClick', function(self,button)
-			if InCombatLockdown() then			
-				if _G.GameMenuFrame:IsShown() then
-					PlaySound(SOUNDKIT.IG_MAINMENU_QUIT);
-					_G.GameMenuFrame:Hide();
-				else
-					PlaySound(SOUNDKIT.IG_MAINMENU_OPEN);
-					_G.GameMenuFrame:Show();
+		button:SetScript("OnMouseDown", function(self, button) 
+			self.isMouseDown = true
+		end)
+		button:SetScript('OnMouseUp', function(self, button)
+			if (self.isMouseDown) then
+				self.isMouseDown = false
+				if InCombatLockdown() then		
+					if not BugSack_LDB_OnClick(self, button) then
+						if _G.GameMenuFrame:IsShown() then
+							PlaySound(SOUNDKIT.IG_MAINMENU_QUIT);
+							HideUIPanel(_G.GameMenuFrame);
+						else
+							PlaySound(SOUNDKIT.IG_MAINMENU_OPEN);
+							ShowUIPanel(_G.GameMenuFrame);
+						end
+					end
+					return
 				end
-				return
-			end
-			
-			if _G.GameMenuFrame:IsShown() then
-				PlaySound(SOUNDKIT.IG_MAINMENU_QUIT);
-				HideUIPanel(_G.GameMenuFrame);
-			else
-				PlaySound(SOUNDKIT.IG_MAINMENU_OPEN);
-				ShowUIPanel(_G.GameMenuFrame);
-			end
 
-			self.isShown = _G["GameMenuFrame"]:IsShown()
-			MB.Events:OnMouse(self)
+				print(button)
+				if not BugSack_LDB_OnClick(self, button) and button == "LeftButton" then
+					if _G.GameMenuFrame:IsShown() then
+						PlaySound(SOUNDKIT.IG_MAINMENU_QUIT);
+						HideUIPanel(_G.GameMenuFrame);
+					else
+						PlaySound(SOUNDKIT.IG_MAINMENU_OPEN);
+						ShowUIPanel(_G.GameMenuFrame);
+					end
+				end
+
+				self.isShown = _G["GameMenuFrame"]:IsShown()
+				MB.Events:OnMouse(self)
+			end
 		end)
 
 		-- Create BugSackNotificationIcon when BugSack addon is loaded
@@ -76,6 +116,12 @@ function MB.MicroButtons:MainMenuMicroButton(button)
 			button.bsNotify.Texture:SetTexture(MB.Utils:GetMicroBarIcon("BugSackNotification"));	
 			button.bsNotify.Texture:SetColorTexture(85/255, 200/255, 85/255,225/255); 
 			hooksecurefunc(BugSack, "UpdateDisplay", function(self)
+				local errors = self:GetErrors(BugGrabber:GetSessionId());
+				if #errors > 0 then
+					button.bsNotify.Texture:SetColorTexture(200/255, 80/255, 80/255,225/255); 
+				end
+			end)
+			hooksecurefunc(BugSack,"Reset", function(self)
 				local errors = self:GetErrors(BugGrabber:GetSessionId());
 				if #errors > 0 then
 					button.bsNotify.Texture:SetColorTexture(200/255, 80/255, 80/255,225/255); 
